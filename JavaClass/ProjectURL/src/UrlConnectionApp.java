2a896362a6669dfc0da8684f9651e2c9f2797f73
fouziahaque <fouziahaque.pr@gmail.com>
2015-10-16 14:17:11 -0400

    Update UrlConnectionApp.java

--- JavaClass/ProjectURL/src/UrlConnectionApp.java
+++ JavaClass/ProjectURL/src/UrlConnectionApp.java
@@ -1,6 +1,7 @@
 import java.io.IOException;
 
 public class UrlConnectionApp {
+	//added this comment
 
 	public static void main(String[] args) throws IOException {
 		String output = ContentReader.getUrlContents("http://www.history.com/topics/constitution");
